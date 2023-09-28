bc58c1e64f31a6d9d63953c8b9b50cf5eeb7ff0a
eesasharma773 <eesa@eesasharma.com>
2023-09-28 18:24:30 -0500

    Second commit

---
+++ src/utils/constants.js
@@ -0,0 +1,41 @@
+import HomeIcon from '@mui/icons-material/Home';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+import Fireplace from '@mui/icons-material/Fireplace';
+import ShoppingBag from '@mui/icons-material/ShoppingBag';
+import MusicNote from '@mui/icons-material/MusicNote';
+import Movie from '@mui/icons-material/Movie';
+import LiveTv from '@mui/icons-material/LiveTv';
+import Games from '@mui/icons-material/Games';
+import Newspaper from '@mui/icons-material/Newspaper';
+import Sports from '@mui/icons-material/Sports';
+import QuestionMark from '@mui/icons-material/QuestionMark';
+import Podcasts from '@mui/icons-material/Podcasts';
+
+
+export const logo = 'https://i.ibb.co/SnjxBnK/Twingrr.png';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon />, },
+  { name: 'Trending', icon: <Fireplace />, },
+  { name: 'Shopping', icon: <ShoppingBag />, },
+  { name: 'Music', icon: <MusicNote />, },
+  { name: 'Movies & TV', icon: <Movie />, },
+  { name: 'Live', icon: <LiveTv />, },
+  { name: 'Gaming', icon: <Games />, },
+  { name: 'News', icon: <Newspaper />, },
+  { name: 'Sports', icon: <Sports />, },
+  { name: 'Learning', icon: <QuestionMark />, },
+  { name: 'Fashion & Beauty', icon: <FaceRetouchingNaturalIcon />, },
+  { name: 'Podcasts', icon: <Podcasts />, },
+  { name: 'Gym', icon: <FitnessCenterIcon />, },
+  { name: 'Crypto', icon: <DeveloperModeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/@YouTube/featured';
+export const demoVideoUrl = '/watch?v=jNQXAC9IVRw';
+export const demoChannelTitle = 'YouTube';
+export const demoVideoTitle = 'Me at the zoo';
+export const demoProfilePicture = 'https://yt3.ggpht.com/zi50weT4GiyJxDfb3BpcmjL3BtOmRWE3gwrmeL43j-mGfj0UF4wjbsyE6i-K_I9Bs7dK0SnCQlY=s48-c-k-c0x00ffffff-no-rj'
